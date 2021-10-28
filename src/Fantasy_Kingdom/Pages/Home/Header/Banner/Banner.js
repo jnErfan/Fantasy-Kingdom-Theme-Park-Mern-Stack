fbef3f2932248342feeab13673db950cc3b34967
jnErfan <j.n.erfan420@gmail.com>
2021-10-29 05:56:15 +0600

    Header Banner  Complete

--- src/Fantasy_Kingdom/Pages/Home/Header/Banner/Banner.js
+++ src/Fantasy_Kingdom/Pages/Home/Header/Banner/Banner.js
@@ -1,10 +1,85 @@
 import React from "react";
+import TypewriterComponent from "typewriter-effect";
 import "./Banner.css";
 
 const Banner = () => {
   return (
     <div className="banner">
-      <div></div>
+      <div className="container">
+        <div className="text-start pt-5 ps-5">
+          <h1 className="headerTitle text-info fw-bold">
+            {" "}
+            <span className="header-title1">Welcome To</span>
+            <span className="text-light">
+              <TypewriterComponent
+                options={{
+                  strings: [
+                    "Fantasy Kingdom",
+                    "Water Kingdom",
+                    "Resort Atlantis",
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+            </span>
+          </h1>
+        </div>
+        <div className="d-flex justify-content-center">
+          <div
+            id="carouselExampleCaptions"
+            className="carousel slide w-75 p-5 animate__animated animate__zoomIn carouselContainer"
+            data-bs-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img
+                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/F_OOR5568-1.jpg"
+                  className="d-block w-100 slider-image"
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/F_OOR5695-1.jpg"
+                  className="d-block w-100 slider-image"
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/H10.jpg"
+                  className="d-block w-100 slider-image"
+                  alt="..."
+                />
+              </div>
+              <div className="carousel-item">
+                <img
+                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/H09.png"
+                  className="d-block  w-100 slider-image"
+                  alt="..."
+                />
+              </div>
+            </div>
+            <button
+              className="carousel-control-prev"
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="prev"
+            >
+              <i className="fas fa-chevron-circle-left sliderLeftButton fs-1"></i>
+            </button>
+            <button
+              className="carousel-control-next"
+              type="button"
+              data-bs-target="#carouselExampleCaptions"
+              data-bs-slide="next"
+            >
+              <i className="fas fa-chevron-circle-right sliderRightButton fs-1"></i>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Fantasy_Kingdom/Pages/Home/Header/Navbar/Navbar.js
+++ src/Fantasy_Kingdom/Pages/Home/Header/Navbar/Navbar.js
@@ -5,6 +5,18 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <div className="bg-dark py-3">
         <Link
           className="mx-4 text-decoration-none text-white fw-bold"
