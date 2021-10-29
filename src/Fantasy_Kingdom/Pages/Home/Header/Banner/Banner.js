9d80f4de9b95b5d94adae154cb246f1f625ad769
jnErfan <j.n.erfan420@gmail.com>
2021-10-30 00:10:58 +0600

    Footer Complete

--- src/Fantasy_Kingdom/Pages/Home/Header/Banner/Banner.js
+++ src/Fantasy_Kingdom/Pages/Home/Header/Banner/Banner.js
@@ -5,91 +5,93 @@ import "./Banner.css";
 const Banner = () => {
   return (
     <div className="banner">
-      <div className="container bannerContainer">
-        <div className="text-start pt-5 ps-5">
-          <h1 className="headerTitle text-info fw-bold">
-            {" "}
-            <span className="header-title1">Welcome To</span>
-            <span className="text-light">
-              <TypewriterComponent
-                options={{
-                  strings: [
-                    "Fantasy Kingdom",
-                    "Water Kingdom",
-                    "Resort Atlantis",
-                  ],
-                  autoStart: true,
-                  loop: true,
-                }}
-              />
-            </span>
-          </h1>
-        </div>
-        <div className="d-flex justify-content-center">
-          <div
-            id="carouselExampleCaptions"
-            className="carousel slide w-75 p-5 animate__animated animate__zoomIn carouselContainer"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <img
-                  src="https://i.ibb.co/nwhj5z7/Fantasy-Kingdom2-1-1.png"
-                  className="d-block w-100 slider-image"
-                  alt="..."
+      <div className="banner2">
+        <div className="container bannerContainer">
+          <div className="text-start pt-5 ps-5">
+            <h1 className="headerTitle text-info fw-bold">
+              {" "}
+              <span className="header-title1">Welcome To</span>
+              <span className="text-light">
+                <TypewriterComponent
+                  options={{
+                    strings: [
+                      "Fantasy Kingdom",
+                      "Water Kingdom",
+                      "Resort Atlantis",
+                    ],
+                    autoStart: true,
+                    loop: true,
+                  }}
                 />
-                <div className="carousel-caption d-none d-md-block">
-                  <h5 className="fw-bold carouselText">
-                    <span className="fantasy">Fantasy Kingdom </span> <br />{" "}
-                    Only Theme Park in Bangladesh
-                  </h5>
+              </span>
+            </h1>
+          </div>
+          <div className="d-flex justify-content-center">
+            <div
+              id="carouselExampleCaptions"
+              className="carousel slide w-75 p-5 animate__animated animate__zoomIn carouselContainer"
+              data-bs-ride="carousel"
+            >
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <img
+                    src="https://i.ibb.co/nwhj5z7/Fantasy-Kingdom2-1-1.png"
+                    className="d-block w-100 slider-image"
+                    alt="..."
+                  />
+                  <div className="carousel-caption d-none d-md-block">
+                    <h5 className="fw-bold carouselText">
+                      <span className="fantasy">Fantasy Kingdom </span> <br />{" "}
+                      Only Theme Park in Bangladesh
+                    </h5>
+                  </div>
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://fantasykingdom.net/wp-content/uploads/2021/08/F_OOR5568-1.jpg"
+                    className="d-block w-100 slider-image"
+                    alt="..."
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://fantasykingdom.net/wp-content/uploads/2021/08/F_OOR5695-1.jpg"
+                    className="d-block w-100 slider-image"
+                    alt="..."
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://fantasykingdom.net/wp-content/uploads/2021/08/H10.jpg"
+                    className="d-block w-100 slider-image"
+                    alt="..."
+                  />
+                </div>
+                <div className="carousel-item">
+                  <img
+                    src="https://fantasykingdom.net/wp-content/uploads/2021/08/H09.png"
+                    className="d-block  w-100 slider-image"
+                    alt="..."
+                  />
                 </div>
               </div>
-              <div className="carousel-item">
-                <img
-                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/F_OOR5568-1.jpg"
-                  className="d-block w-100 slider-image"
-                  alt="..."
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/F_OOR5695-1.jpg"
-                  className="d-block w-100 slider-image"
-                  alt="..."
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/H10.jpg"
-                  className="d-block w-100 slider-image"
-                  alt="..."
-                />
-              </div>
-              <div className="carousel-item">
-                <img
-                  src="https://fantasykingdom.net/wp-content/uploads/2021/08/H09.png"
-                  className="d-block  w-100 slider-image"
-                  alt="..."
-                />
-              </div>
+              <button
+                className="carousel-control-prev"
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="prev"
+              >
+                <i className="fas fa-chevron-circle-left sliderLeftButton fs-1"></i>
+              </button>
+              <button
+                className="carousel-control-next"
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide="next"
+              >
+                <i className="fas fa-chevron-circle-right sliderRightButton fs-1"></i>
+              </button>
             </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="prev"
-            >
-              <i className="fas fa-chevron-circle-left sliderLeftButton fs-1"></i>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="next"
-            >
-              <i className="fas fa-chevron-circle-right sliderRightButton fs-1"></i>
-            </button>
           </div>
         </div>
       </div>
--- src/Fantasy_Kingdom/Pages/Shared/Footer.js
+++ src/Fantasy_Kingdom/Pages/Shared/Footer.js
@@ -4,99 +4,102 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footerContainer">
-      <div className="container">
-        <div>
-          <div className="row row-cols-3 pt-5">
-            <div className="col col-12 col-md-6 col-lg-4 text-start firstColum">
-              <h2 className="fontTeko mb-4">CONTACT US </h2>
-              <p>
-                <small>
-                  If you have any questions, please contact us at
-                  info@fantasykingdom.net
-                </small>
-              </p>
-              <h1 className="fontTeko mt-4">
-                <i className="fas fa-phone-alt me-3"></i> 09612-002020
-              </h1>
-            </div>
-            <div className="col col-12 col-md-6 col-lg-4">
-              <div className="d-block">
-                <Link className="text-light footer_nav d-block my-2" to="/">
-                  About Us
-                </Link>{" "}
-                <Link
-                  className="text-light footer_nav  d-block my-2"
-                  to="/contract"
-                >
-                  Contract Us
-                </Link>{" "}
-                <Link className="text-light footer_nav d-block my-2" to="/">
-                  Get Help
-                </Link>{" "}
-                <Link className="text-light footer_nav  d-block my-2" to="/">
-                  Support
-                </Link>{" "}
-                <Link className="text-light footer_nav  d-block my-2" to="/">
-                  Report A Bug
-                </Link>{" "}
-                <Link
-                  className="text-light footer_nav  d-block my-2"
-                  to="/contract"
-                >
-                  Terms Privacy
-                </Link>
+    <div className="footerContainer2">
+      <div className="footerContainer">
+        <div className="container">
+          <div>
+            <div className="row row-cols-3 pt-5">
+              <div className="col col-12 col-md-6 col-lg-4 text-start firstColum">
+                <h2 className="fontTeko mb-4">CONTACT US </h2>
+                <p>
+                  <small>
+                    If you have any questions, please contact us at
+                    info@fantasykingdom.net
+                  </small>
+                </p>
+                <h1 className="fontTeko mt-4">
+                  <i className="fas fa-phone-alt me-3"></i> 09612-002020
+                </h1>
               </div>
-            </div>
-            <div className="col col-12 col-md-6 col-lg-4 thirdColum">
-              <div className="text-start ps-2 ">
-                <div>
-                  <h1 className="fontTeko">PAYMENT WITH</h1>
-                  <img
-                    className="w-100"
-                    src="https://fantasykingdom.net/wp-content/uploads/2020/10/cardinfo.png"
-                    alt=""
-                  />
+              <div className="col col-12 col-md-6 col-lg-4">
+                <div className="d-block">
+                  <Link className="text-light footer_nav d-block my-2" to="/">
+                    About Us
+                  </Link>{" "}
+                  <Link
+                    className="text-light footer_nav  d-block my-2"
+                    to="/contract"
+                  >
+                    Contract Us
+                  </Link>{" "}
+                  <Link className="text-light footer_nav d-block my-2" to="/">
+                    Get Help
+                  </Link>{" "}
+                  <Link className="text-light footer_nav  d-block my-2" to="/">
+                    Support
+                  </Link>{" "}
+                  <Link className="text-light footer_nav  d-block my-2" to="/">
+                    Report A Bug
+                  </Link>{" "}
+                  <Link
+                    className="text-light footer_nav  d-block my-2"
+                    to="/contract"
+                  >
+                    Terms Privacy
+                  </Link>
                 </div>
-                <div className="mt-5 d-flex">
-                  <input
-                    className="inputField"
-                    placeholder="Your Email"
-                    type="email"
-                  />
-                  <input
-                    type="submit"
-                    className="buttonField btn"
-                    value="Subscribe"
-                  />
+              </div>
+              <div className="col col-12 col-md-6 col-lg-4 thirdColum">
+                <div className="text-start ps-2 ">
+                  <div>
+                    <h1 className="fontTeko">PAYMENT WITH</h1>
+                    <img
+                      className="w-100"
+                      src="https://fantasykingdom.net/wp-content/uploads/2020/10/cardinfo.png"
+                      alt=""
+                    />
+                  </div>
+                  <div className="mt-5 d-flex">
+                    <input
+                      className="inputField"
+                      placeholder="Your Email"
+                      type="email"
+                      required
+                    />
+                    <input
+                      type="submit"
+                      className="buttonField btn"
+                      value="Subscribe"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div>
-          <hr />
-          <div className="footar-icon text-center mt-3">
-            <a href="https://www.facebook.com/jnerfan">
-              {" "}
-              <i className="fab fa-facebook fb pe-3"></i>{" "}
-            </a>
-            <a href="https://www.instagram.com/j.n.erfan/">
-              <i className="fab fa-instagram ins pe-3"></i>
-            </a>
-            <a href="https://twitter.com/JNErfan">
-              <i className="fab fa-twitter twt pe-3"></i>
-            </a>
-            <a href="https://www.youtube.com/channel/UCRJ5d8nP2PBmv6rYdtNfFag">
-              <i className="fab fa-youtube yt"></i>
-            </a>
-          </div>
+          <div>
+            <hr />
+            <div className="footar-icon text-center mt-3">
+              <a href="https://www.facebook.com/jnerfan">
+                {" "}
+                <i className="fab fa-facebook fb pe-3"></i>{" "}
+              </a>
+              <a href="https://www.instagram.com/j.n.erfan/">
+                <i className="fab fa-instagram ins pe-3"></i>
+              </a>
+              <a href="https://twitter.com/JNErfan">
+                <i className="fab fa-twitter twt pe-3"></i>
+              </a>
+              <a href="https://www.youtube.com/channel/UCRJ5d8nP2PBmv6rYdtNfFag">
+                <i className="fab fa-youtube yt"></i>
+              </a>
+            </div>
 
-          <div className="py-2">
-            <small>
-              <i class="far fa-copyright"></i> Design With Love By{" "}
-              <span className="text-danger fw-bold"> J.N.Erfan</span>
-            </small>
+            <div className="py-2">
+              <small>
+                <i class="far fa-copyright"></i> Design With Love By{" "}
+                <span className="text-danger fw-bold"> J.N.Erfan</span>
+              </small>
+            </div>
           </div>
         </div>
       </div>
