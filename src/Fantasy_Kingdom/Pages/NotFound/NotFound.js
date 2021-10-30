4db4bab79c8b1524f17c717c3fb08a966881e5f5
jnErfan <j.n.erfan420@gmail.com>
2021-10-30 15:50:34 +0600

    404 Page Not Found  Done

--- src/Fantasy_Kingdom/Pages/NotFound/NotFound.js
+++ src/Fantasy_Kingdom/Pages/NotFound/NotFound.js
@@ -3,8 +3,13 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className="mt-5">
-      <h1>404</h1>
-      <p>Page Not Found</p>
+      <h1 className="text-danger fw-bold">404</h1>
+      <p className="text-danger fw-bold">Page Not Found</p>
+      <img
+        className="w-50"
+        src="https://i.ibb.co/0X6b0Bp/404-Error.gif"
+        alt=""
+      />
     </div>
   );
 };
